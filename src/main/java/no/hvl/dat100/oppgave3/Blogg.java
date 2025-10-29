c0e85ff4715eca8fd42f8b0516d44aea56da8ee8
669773 <669773@stud.hvl.no>
2025-10-29 16:20:40 +0100

    kommet til 3i

--- src/main/java/no/hvl/dat100/oppgave3/Blogg.java
+++ src/main/java/no/hvl/dat100/oppgave3/Blogg.java
@@ -2,7 +2,7 @@ package no.hvl.dat100.oppgave3;
 
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave1.*;
-
+import no.hvl.dat100.oppgave2.*;
 public class Blogg {
 
 	//objektvariable
@@ -28,31 +28,57 @@ public class Blogg {
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return innleggtabell;
 
 	}
-	
-	public int finnInnlegg(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+	public int finnInnlegg(Innlegg innlegg) {
+		for (int i = 0; i < innleggtabell.length; i++) {
+			if (innleggtabell[i] != null && innleggtabell[i].erLik(innlegg)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+			for (int i = 0; i < innleggtabell.length; i++) {
+				if (innleggtabell[i] != null && innleggtabell[i].erLik(innlegg)) {
+					return true;
+				}
+			}
+				return false;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		for (int i = 0; i < innleggtabell.length; i++) {
+			if (innleggtabell[i] == null) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		if(ledigPlass() && !finnes(innlegg)){
+			for(int i=0;i<innleggtabell.length;i++){
+				if(innleggtabell[i] == null){
+					innleggtabell[i] = innlegg;
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		int count = 0;
+		for(int i =0;i<innleggtabell.length;i++){
+			if(innleggtabell[i] !=null){
+				count ++;
+			}
+		}
+		return count+Innlegg.Tekst.toString();
 	}
 
 	// valgfrie oppgaver nedenfor
